f04125ce577e100a58254ffa4b21f63b24122bbc
Robert_Bohdziewicz <Robert_Bohdziewicz@epam.com>
2022-10-18 14:47:10 +0200

    Lets play with shit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,16 @@
-node { // node/agent
-    stage('Stage 1') {
-        echo 'Hello World' // echo Hello World
+pipeline {
+    agent any
+
+    environment {
+        USE_JDK = 'true'
+        JDK ='c:/Java/jdk1.8'
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'printenv'
+            }
+        }
     }
 }
